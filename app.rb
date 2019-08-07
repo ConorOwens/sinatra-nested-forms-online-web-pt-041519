f1dd10f15f06db50adf357c628ec0636a5588c00
Conor Owens <conorj.owens@gmail.com>
2019-08-07 02:24:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ module FormsLab
       
       params[:pirate][:ships].each do |details|
         binding.pry
-        Ship.new(details)
+        Ship.new(details[:])
       end
       @ships = Ships.all
       erb :'pirates/show'
